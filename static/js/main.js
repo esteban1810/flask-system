bde00b4cbc31467aae35c431c9785cf9f83b94f8
Francisco López Sánchez <francisco_soccer64@hotmail.com>
2021-09-10 21:21:30 -0500

    correccion3

---
+++ static/js/main.js
@@ -0,0 +1,21 @@
+
+
+function mostrarCedula(){
+    if(document.getElementById('medico').value === '2'){
+        document.getElementById('cedula').style.display = 'block';
+    }
+}
+
+function ocultarCedula(){
+    if(document.getElementById('administrador').value === '1'){
+        document.getElementById('cedula').style.display = 'none';
+    }
+}
+
+//
+function validarPassword(){
+    if(document.getElementById('confPass').value != document.getElementById('pass').value){
+        alert("Las contraseñas no coinciden");
+        return false;
+    }
+}
